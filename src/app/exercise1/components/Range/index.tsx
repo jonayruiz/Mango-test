fb80a8d5fde36f6a44bab566aadd831901dee678
jonay <138667539+jonayxferacf@users.noreply.github.com>
2024-12-10 23:56:13 +0000

    fixes

--- src/app/exercise1/components/Range/index.tsx
+++ src/app/exercise1/components/Range/index.tsx
@@ -4,11 +4,8 @@ import React, { useRef, useState, useEffect } from "react";
 const Range = () => {
   const [minValue, setMinValue] = useState<number>(1);
   const [maxValue, setMaxValue] = useState<number>(100);
-  const [rangeMin, setRangeMin] = useState<number>(minValue);
-  const [rangeMax, setRangeMax] = useState<number>(maxValue);
-
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState<any>()
+  const [rangeMin, setRangeMin] = useState<number>(2);
+  const [rangeMax, setRangeMax] = useState<number>(40);
 
   const [mappedMinHandle, setMappedMinHandle] = useState(0);
   const [mappedMaxHandle, setMappedMaxHandle] = useState(0);
@@ -17,8 +14,9 @@ const Range = () => {
   const currentHandle = useRef<string | null>(null);
 
   const refContainer = useRef<HTMLInputElement>(null);
-  const refThumb = useRef<HTMLInputElement>(null);
-
+  const [error, setError] = useState<boolean>(false);
+  const [loading,setLoading] = useState<boolean>(true)
+  
   useEffect(() => {
     const fetchResponse = async () => {
       try {
@@ -34,26 +32,20 @@ const Range = () => {
           setRangeMax(data.max);
         }
       } catch (err) {
-        setError(err)
+         setError(true)
       } finally {
-        setLoading(false)
+         setLoading(false)
       }
     };
 
     fetchResponse();
   }, []);
 
-  const handleMouseMove = (e: any) => {
-    if (
-      !isDragging.current ||
-      !currentHandle.current ||
-      !refContainer.current ||
-      !refThumb.current
-    )
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isDragging.current || !currentHandle.current || !refContainer.current)
       return;
 
     const rect = refContainer.current.getBoundingClientRect();
-    // const rectThumb = refThumb.current.getBoundingClientRect();
     const rectLeft = rect.left;
     const rectWidth = rect.width;
     const clickPosition = e.clientX - rectLeft;
@@ -63,9 +55,11 @@ const Range = () => {
     );
 
     if (currentHandle.current === "min") {
-      if (newValue < rangeMax && newValue >= minValue) setRangeMin(newValue);
+      if (newValue >= minValue && newValue < rangeMax)
+        setRangeMin(Math.max(minValue, Math.min(newValue, rangeMax)));
     } else {
-      if (newValue > rangeMin && newValue <= maxValue) setRangeMax(newValue);
+      if (newValue > rangeMin && newValue <= maxValue)
+        setRangeMax(Math.min(maxValue, Math.max(newValue, rangeMin)));
     }
   };
 
@@ -86,69 +80,84 @@ const Range = () => {
   useEffect(() => {
     setMappedMinHandle(((rangeMin - minValue) / (maxValue - minValue)) * 100);
     setMappedMaxHandle(((rangeMax - minValue) / (maxValue - minValue)) * 100);
+    setRangeMin((prev) => Math.max(minValue, Math.min(prev, rangeMax)));
+    setRangeMax((prev) => Math.min(maxValue, Math.max(prev, rangeMin)));
+  }, [minValue, maxValue, rangeMax, rangeMin]);
 
-    if (rangeMin < minValue || rangeMin >= rangeMax) {
-      setRangeMin(Math.min(minValue, rangeMax - 1));
+  const validateInput = (value: number, isMin: boolean) => {
+    if (isMin) {
+      return Math.min(value, maxValue - 1);
+    } else {
+      return Math.max(value, minValue + 1);
     }
-
-    if (rangeMax > maxValue || rangeMax <= rangeMin) {
-      setRangeMax(Math.max(rangeMin + 1, maxValue));
-    }
-  }, [rangeMax, rangeMin, minValue, maxValue]);
-
-
+  };
+  const hasError = error ? <div className="text-red-500">Error fetching data</div> : null
   return (
-    <div className="w-full relative ">
-      {!loading?<><input
-        type="number"
-        value={rangeMin}
-        min={minValue}
-        max={rangeMax}
-        className="absolute top-1/2 left-0 -translate-x-1/2 w-12  -translate-y-1/2 bg-transparent text-center border-2 border-white rounded-md outline-white"
-        onChange={(e) => {
-          const value = Number(e.target.value);
-          setRangeMin(value);
-        }}
-   
-      />
-      <input
-        type="number"
-        value={rangeMax}
-        min={rangeMin}
-        max={maxValue}
-        className="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2 w-12 bg-transparent text-center border-2 border-white rounded-md outline-white"
-        onChange={(e) => {
-          const value = Number(e.target.value);
-          setRangeMax(value);
-        }}
-   
-      /></>:''}
+    <>
+    {hasError}
+    {loading?'Loading...':
+    <div className="w-full relative">
+      <>
+        <input
+          type="number"
+          value={minValue}
+          min={minValue}
+          max={rangeMax}
+          className="absolute top-1/2 left-0 -translate-x-1/2 w-12  -translate-y-1/2 bg-transparent text-center border-2 border-white rounded-md outline-white"
+          onChange={(e) => {
+            const value = Number(e.target.value);
+            setMinValue(validateInput(value, true));
+          }}
+        />
+        <input
+          type="number"
+          min={minValue}
+          max={rangeMax}
+          value={maxValue}
+          className="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2 w-12 bg-transparent text-center border-2 border-white rounded-md outline-white"
+          onChange={(e) => {
+            const value = Number(e.target.value);
+            setMaxValue(validateInput(value, false));
+          }}
+        />
+      </>
 
       <div ref={refContainer} className="w-[calc(100%-120px)] mx-auto relative">
         <div className="h-4 w-full bg-white rounded-full" />
-      {!loading?  <>
-        <div
-          className="absolute  p-4 top-1/2 -translate-y-1/2 -translate-x-1/2  transition-all ease-out duration-200 cursor-grab  active:cursor-grabbing group"
-          style={{
-            left: `${mappedMaxHandle}%`,
-          }}
-          onMouseDown={(e) => startDrag(e, "max")}
-        >
-          <div className="w-6 h-6 bg-black rounded-full shadow-sm shadow-white border-2 transition-transform hover:scale-125"></div>
-        </div>
-        <div
-          ref={refThumb}
-          className="absolute p-4 top-1/2 -translate-y-1/2  -translate-x-1/2 transition-all ease-out duration-200 cursor-grab  active:cursor-grabbing group"
-          onMouseDown={(e) => startDrag(e, "min")}
-          style={{
-            left: `${mappedMinHandle}%`,
-          }}
-        >
-          <div className="w-6 h-6 bg-black rounded-full shadow-sm border-white border-2 shadow-white transition-transform hover:scale-125 group-active:scale-125"></div>
-        </div>
-        </>:''}
+        <>
+          <div
+            className="absolute z-20 top-1/2 -translate-y-1/2 -translate-x-1/2  transition-transform ease-out duration-200 cursor-grab  active:cursor-grabbing group"
+            style={{
+              left: `${mappedMaxHandle}%`,
+            }}
+            onMouseDown={(e) => startDrag(e, "max")}
+          >
+            <div className="absolute -translate-y-full left-1/2 -translate-x-1/2 select-none">
+              {rangeMax}
+            </div>
+
+            <div className="w-6 h-6 bg-black rounded-full shadow-sm shadow-white border-2 transition-transform hover:scale-125"></div>
+          </div>
+          <div
+            className="absolute top-1/2 -translate-y-1/2  -translate-x-1/2 transition-transform ease-out duration-200 cursor-grab  active:cursor-grabbing group"
+            onMouseDown={(e) => startDrag(e, "min")}
+            style={{
+              left: `${mappedMinHandle}%`,
+              transform: `translate(-50%, -50%) scale(${
+                rangeMax - rangeMin < 3 ? 2 : 1
+              })`,
+            }}
+          >
+            <div className="absolute -translate-y-full left-1/2 -translate-x-1/2 select-none">
+              {rangeMin}
+            </div>
+            <div className="w-6 h-6 bg-black rounded-full shadow-sm border-white border-2 shadow-white transition-transform hover:scale-125 group-active:scale-125"></div>
+          </div>
+        </>
       </div>
     </div>
+    }
+    </>
   );
 };
 
--- src/app/exercise2/components/Range/index.tsx
+++ src/app/exercise2/components/Range/index.tsx
@@ -6,13 +6,37 @@ const Range = ({ steps }: { steps: number[] }) => {
   const [rangeValues, setRangeValues] = useState<number[]>(sortedSteps);
   const [minIndex, setMinIndex] = useState(0);
   const [maxIndex, setMaxIndex] = useState(sortedSteps.length - 1);
-  const [minLabel, setMinLabel] = useState(sortedSteps[0]);
-  const [maxLabel, setMaxLabel] = useState(sortedSteps[sortedSteps.length - 1]);
+
   const isDragging = useRef(false);
   const currentHandle = useRef<string | null>(null);
 
   const containerRef = useRef<HTMLDivElement>(null);
 
+  const [error, setError] = useState<boolean>(false);
+  const [loading,setLoading] = useState<boolean>(true)
+  
+  // useEffect(() => {
+  //   const fetchResponse = async () => {
+  //     try {
+  //       const response = await fetch("https://demo9065139.mockable.io/range");
+  //       if (!response.ok) {
+  //         throw Error("No response");
+  //       } else {
+  //         const data = await response.json();
+  //         console.log(data);
+  //         setMinIndex(data[0]);
+  //         setMaxIndex(data[data.length - 1]);
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //        setError(true)
+  //     } finally {
+  //        setLoading(false)
+  //     }
+  //   };
+
+  //   fetchResponse();
+  // }, []);
 
   const handleMouseMove = (e: MouseEvent) => {
     if (!isDragging.current || !currentHandle.current || !containerRef.current)
@@ -50,20 +74,25 @@ const Range = ({ steps }: { steps: number[] }) => {
         ref={containerRef}
         className="relative w-[calc(100%-120px)] h-4 bg-gray-300 rounded-full"
       >
+        <div className="absolute pointer-events-none top-0 -translate-y-full pb-2 -left-[4%]  w-[108%] flex justify-between">
+        {rangeValues.map((o, i) => {
+          return <div key={`rangeValues-${i}`} className="relative select-none">{o}</div>
+        })}
+        </div>
         <input
           type="number"
-          value={minLabel}
+          value={steps[minIndex]}
           readOnly
-          className="absolute pointer-events-none select-none selection:bg-transparent top-1/2 -left-12 -translate-x-1/2 w-12  -translate-y-1/2 bg-transparent text-center outline-white"
+          className="absolute pointer-events-none select-none selection:bg-transparent top-1/2 -left-12  -translate-x-1/2 w-12  -translate-y-1/2 bg-transparent text-center outline-white"
         />
         <input
           type="number"
-          value={maxLabel}
+          value={steps[maxIndex]}
           className="absolute  pointer-events-none select-none selection:bg-transparent top-1/2 -right-12 translate-x-1/2 -translate-y-1/2 w-12 bg-transparent text-center  outline-white"
           readOnly
         />
         <div
-          className="absolute  p-4 top-1/2 -translate-y-1/2 -translate-x-1/2  transition-all ease-out duration-200 cursor-grab  active:cursor-grabbing group"
+          className="absolute  p-4 top-1/2 -translate-y-1/2 -translate-x-1/2  transition-transform ease-out duration-200 cursor-grab  active:cursor-grabbing group"
           style={{
             left: `${(minIndex / (rangeValues.length - 1)) * 100}%`,
         
@@ -74,7 +103,7 @@ const Range = ({ steps }: { steps: number[] }) => {
           <div className="w-6 h-6 bg-black rounded-full shadow-sm border-white border-2 shadow-white transition-transform hover:scale-125 group-active:scale-125"></div>
         </div>
         <div
-          className="absolute p-4 top-1/2 -translate-y-1/2  -translate-x-1/2 transition-all ease-out duration-200 cursor-grab  active:cursor-grabbing group"
+          className="absolute p-4 top-1/2 -translate-y-1/2  -translate-x-1/2 transition-transform ease-out duration-200 cursor-grab  active:cursor-grabbing group"
           style={{
             left: `${(maxIndex / (rangeValues.length - 1)) * 100}%`,
            
