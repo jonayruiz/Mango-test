7c9e9d248b966ebcaa32a70f20077ec517e9a003
jonay <jonaygonzalezruiz@gmail.com>
2024-12-11 09:47:02 +0000

    rm comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +16,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`font-mono antialiased`}
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
        <Header />
         {children}
